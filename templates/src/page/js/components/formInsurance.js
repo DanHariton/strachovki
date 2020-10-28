62284c1af4bb52779cf04e45a9a34acc7181c45b
DanHariton <danilhariton@gmail.com>
2020-10-28 23:43:21 +0100

    fix fe, responsive

--- templates/src/page/js/components/formInsurance.js
+++ templates/src/page/js/components/formInsurance.js
@@ -449,12 +449,13 @@ $('#insurance_startDate').change(function () {
     if (insuranceSelected === INSURANCE_MAXIMA) {
         if (moment($('#insurance_startDate').val()).month() < moment().month()) {
             $('#insurance_startDate').val(moment().format('YYYY-MM-DD'));
-            alert('Пожулуйста, выберите актуальную дату!');
+            alert('Пожулуйста, выберите актуальную дату!\n' +
+            'Если вы хотите выбрать прошедшую дату, свяжитесь с нами для уточнения информации');
         }
     } else {
         if (moment($('#insurance_startDate').val()) < moment()) {
             $('#insurance_startDate').val(moment().format('YYYY-MM-DD'));
-            alert('Пожалуйста, берите актуальную дату!\n' +
+            alert('Пожалуйста, выберите актуальную дату!\n' +
                 'Если вы хотите выбрать прошедшую дату, свяжитесь с нами для уточнения информации');
         }
     }
